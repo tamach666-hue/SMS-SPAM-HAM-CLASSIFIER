08b66bca387cbaa0cf5135a176bec16f38cb79a4
tamach666-hue <71974937+tamach666-hue@users.noreply.github.com>
2020-11-08 14:06:11 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask, render_template, url_for, request
 import pandas as pd 
-import numpy as np
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
 
 app = Flask(__name__)
 filename = 'sms_model.pkl'
@@ -12,25 +12,26 @@ cv=pickle.load(open('tranform.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
+    NB_spam_model = open('sms_model.pkl','rb')
+    clf = joblib.load(NB_spam_model)
+    
+    cv_model = open('tranform.pkl', 'rb')
+    cv = joblib.load(cv_model)
+    
     if request.method == 'POST':
-		message = request.form['sample_text']
-		data = [message]
-		vect = cv.transform(data).toarray()
-		sms_prediction = nbclf.predict(vect)
-		return render_template('result.html', prediction=sms_prediction)
-      
-	    sms_prediction = str(sms_prediction[0])
-		if sms_prediction == '1':
-			return "This is a spam message"
-		else:
-			return "This a ham message."
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+	
+    return render_template('result.html',prediction = my_prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
